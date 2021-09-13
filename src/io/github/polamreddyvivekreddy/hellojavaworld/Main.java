dfb565eba3aee3551eed24e9060d776ac203e966
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-09-13 18:42:13 +0530

    Order of execution of Threads cannot be predicted.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -3,14 +3,20 @@ package io.github.polamreddyvivekreddy.hellojavaworld;
 public class Main {
 
     public static void main(String[] args) {
-        // We need not use new Thread() because this class already extended Thread
-        ThreadByExtends threadByExtends = new ThreadByExtends();
-        // Thread.start() will call Thread.run()
-        threadByExtends.start();
 
-        // We need to use new Thread(Runnable) to convert to Thread
-        Thread threadByImplements = new Thread(new ThreadByImplements());
-//        Thread.start() will call Thread.run()
-        threadByImplements.start();
+        // Order of execution of Threads cannot be predicted. Below is the example.
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            // We need not use new Thread() because this class already extended Thread
+            ThreadByExtends threadByExtends = new ThreadByExtends();
+            // Thread.start() will call Thread.run()
+            threadByExtends.start();
+
+            // We need to use new Thread(Runnable) to convert to Thread
+            Thread threadByImplements = new Thread(new ThreadByImplements());
+            // Thread.start() will call Thread.run()
+            threadByImplements.start();
+        }
+
     }
 }
