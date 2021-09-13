5325ae91ad8b50cdc8848c4df9bf21d66ae3e64c
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-09-13 18:29:17 +0530

    Initial commit

---
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -0,0 +1,9 @@
+package io.github.polamreddyvivekreddy.hellojavaworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello-java-world");
+    }
+}
