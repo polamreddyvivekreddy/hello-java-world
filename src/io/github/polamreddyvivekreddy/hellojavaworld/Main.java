ea1242822d2f2748eed6ec1c80f16c56e1f349cc
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-09-14 16:34:44 +0530

    Thread priority demo.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -3,14 +3,45 @@ package io.github.polamreddyvivekreddy.hellojavaworld;
 public class Main {
 
     public static void main(String[] args) {
-        // We need not use new Thread() because this class already extended Thread
-        ThreadByExtends threadByExtends = new ThreadByExtends();
-        // Thread.start() will call Thread.run()
-        threadByExtends.start();
+
 
         // We need to use new Thread(Runnable) to convert to Thread
-        Thread threadByImplements = new Thread(new ThreadByImplements());
-//        Thread.start() will call Thread.run()
-        threadByImplements.start();
+        Thread threadByImplements1 = new Thread(new ThreadByImplements("Thread1"));
+        // Thread.start() will call Thread.run()
+        threadByImplements1.setPriority(Thread.NORM_PRIORITY); //5
+
+
+        Thread threadByImplements2 = new Thread(new ThreadByImplements("Thread2"));
+        threadByImplements2.setPriority(Thread.MIN_PRIORITY); //1 and never <1
+
+        Thread threadByImplements3 = new Thread(new ThreadByImplements("Thread3"));
+        threadByImplements3.setPriority(Thread.MAX_PRIORITY); //10 and never >10
+
+        Thread threadByImplements4 = new Thread(new ThreadByImplements("Thread4"));
+        threadByImplements4.setPriority(4); // range is 1-10 inclusive
+
+        System.out.println(threadByImplements1.getPriority());
+        System.out.println(threadByImplements2.getPriority());
+        System.out.println(threadByImplements3.getPriority());
+        System.out.println(threadByImplements4.getPriority());
+
+        threadByImplements1.start();
+        threadByImplements2.start();
+        threadByImplements3.start();
+        threadByImplements4.start();
+
+        // main thread has priority 5 by default
+        Thread.currentThread().getName();
+        Thread.currentThread().getPriority();
+
+        // TODO : Find an example to show evident difference
+
+        // TODO : Daemon Thread demo
+
+        /*You will see that the order might not be inline with priority because
+        currently there is no enough load on CPU but this behaviour will
+        be evident when you have large data processing*/
+
+
     }
 }
