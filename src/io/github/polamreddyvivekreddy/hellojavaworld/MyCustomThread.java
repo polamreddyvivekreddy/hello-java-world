b65a30f3ee70b62f08084ca7bba8baad49272572
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-09-15 10:02:11 +0530

    Thread synchronized block demo.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/MyCustomThread.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/MyCustomThread.java
@@ -9,14 +9,30 @@ public class MyCustomThread implements Runnable{
     }
 
     @Override
-    /*Without synchronized keyword
-    you can see the order of threads are jumbled*/
-//    public void run() {
-    public synchronized void run() {
-        for(int i=1;i<=10;i++){
-            int result = i*number;
-            System.out.println(Thread.currentThread().getName() + ":" +
-                    i + " * " + number + " = " + result);
+
+    /*When method has some operations which doesn't require synchronization,
+    it is wise to synchronize only that particular block for effective usage of CPU*/
+    public void run() {
+
+        // Non-sensitive data below for multiple threads, no synchronization required
+        System.out.println("hello, I am called : " + Thread.currentThread().getName());
+
+        // sensitive data below for multiple threads
+        // this means the current object
+        synchronized (this){
+
+            System.out.println("welcome " + Thread.currentThread().getName() +  ", to synchronized block of printing multiples \n "  +
+            "other threads cannot access printing of multiples before I complete my work ");
+
+            for(int i=1;i<=10;i++){
+                int result = i*number;
+                System.out.println(Thread.currentThread().getName() + ":" +
+                        i + " * " + number + " = " + result);
+            }
         }
+
+        // Non-sensitive data below for multiple threads, no synchronization required
+        System.out.println("bye, I reached end of code : " + Thread.currentThread().getName());
+
     }
 }
