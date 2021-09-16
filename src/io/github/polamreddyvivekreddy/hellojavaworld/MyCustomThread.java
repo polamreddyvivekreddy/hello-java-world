75b34d39a5bb87c6f7a09ee3c246fa99025a6696
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-09-16 10:12:03 +0530

    Thread ReentrantLock demo.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/MyCustomThread.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/MyCustomThread.java
@@ -1,35 +1,42 @@
 package io.github.polamreddyvivekreddy.hellojavaworld;
 
-public class MyCustomThread implements Runnable{
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyCustomThread implements Runnable {
 
     private int number;
+
+    //Lock is an interface, ReentrantLock is the implementation
+    private Lock locker = new ReentrantLock();
 
     public MyCustomThread(int number) {
         this.number = number;
     }
 
     @Override
-
-    /*When method has some operations which doesn't require synchronization,
-    it is wise to synchronize only that particular block for effective usage of CPU*/
     public void run() {
 
+
+
         // Non-sensitive data below for multiple threads, no synchronization required
-        System.out.println("hello, I am called : " + Thread.currentThread().getName());
+        System.out.println("hello, I begin the journey : " + Thread.currentThread().getName());
 
         // sensitive data below for multiple threads
-        // this means the current object
-        synchronized (this){
+        /* lock can help in synchronization especially
+         when you have a series of synchronized and non-synchronized block.
+         In such cases, using many synchronized blocks makes code look ugly and also error prone */
+        locker.lock();
+        System.out.println("welcome " + Thread.currentThread().getName() + ", after locker.lock() for printing multiples \n " +
+                "other threads cannot access printing of multiples before I complete my multiples ");
 
-            System.out.println("welcome " + Thread.currentThread().getName() +  ", to synchronized block of printing multiples \n "  +
-            "other threads cannot access printing of multiples before I complete my work ");
-
-            for(int i=1;i<=10;i++){
-                int result = i*number;
-                System.out.println(Thread.currentThread().getName() + ":" +
-                        i + " * " + number + " = " + result);
-            }
+        for (int i = 1; i <= 10; i++) {
+            int result = i * number;
+            System.out.println(Thread.currentThread().getName() + ":" +
+                    i + " * " + number + " = " + result);
         }
+        locker.unlock();
+
 
         // Non-sensitive data below for multiple threads, no synchronization required
         System.out.println("bye, I reached end of code : " + Thread.currentThread().getName());
