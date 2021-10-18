83af854eac461a6799203ef26cc3981650c57328
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-10-18 10:30:03 +0530

    Custom Generics Class example.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -7,16 +7,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Vivek");
-        stringList.add("24");
+        OurCustomGeneric<String> ourCustomGeneric1 = new OurCustomGeneric<>();
+        ourCustomGeneric1.setOurCustomObject("Vivek");
+//        ourCustomGeneric1.setOurCustomObject(24); // Won't work
+        ourCustomGeneric1.getOurCustomObject();
 
-        // Doesn't allow even at compile time
-//        stringList.add(24);
+        OurCustomGeneric<Integer> ourCustomGeneric2 = new OurCustomGeneric<>();
+        ourCustomGeneric2.setOurCustomObject(24);
+//        ourCustomGeneric2.setOurCustomObject("Vivek"); // Won't work
+        ourCustomGeneric2.getOurCustomObject();
 
-        String name = stringList.get(0);
-
-        // Doesn't allow even at compile time
-//        int age = stringList.get(1);
     }
 }
+
+/**
+ * 1. T -> Type
+ * 2. E -> Element
+ * 3. K -> Key
+ * 4. N -> Number
+ * 5. V -> Value
+ *
+ */
+
+class OurCustomGeneric<T>{
+    private T ourCustomObject;
+
+    public T getOurCustomObject() {
+        return ourCustomObject;
+    }
+
+    public void setOurCustomObject(T ourCustomObject) {
+        this.ourCustomObject = ourCustomObject;
+    }
+}
+
