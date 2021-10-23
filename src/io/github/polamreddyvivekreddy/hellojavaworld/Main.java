493b9a60a2538d5c191f2eab9eee039875e474cd
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-10-23 16:09:58 +0530

    Consumer Functional Interface demo.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -1,9 +1,35 @@
 package io.github.polamreddyvivekreddy.hellojavaworld;
 
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello-java-world");
+
+        /*Consumer<String> consumer = string -> System.out.println(string);
+        consumer.accept("hello consumer");*/
+
+        Consumer<Person> consumer = person -> System.out.println(person);
+        consumer.accept(new Person("vivek","21"));
+
+
+    }
+}
+class Person{
+    String name;
+    String age;
+
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
     }
 }
