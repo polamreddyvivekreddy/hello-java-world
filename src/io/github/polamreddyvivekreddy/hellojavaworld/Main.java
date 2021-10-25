504d9635b940f6b5de34107b56bb01611650098e
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2021-10-25 09:17:03 +0530

    stream interface demo.

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -1,9 +1,49 @@
 package io.github.polamreddyvivekreddy.hellojavaworld;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello-java-world");
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("timon");
+        stringList.add("&&&&&&");
+        stringList.add("pumba");
+        stringList.add("pumba");
+
+        // forEach
+        /*System.out.println(stringList);
+        stringList.forEach(name -> System.out.println(name));
+        stringList.forEach(System.out::println);*/
+
+        // distinct
+        /*Stream<String> stringStream = stringList.stream().distinct();
+        stringStream.forEach(System.out::println);
+        stringStream.close();*/
+
+        // map
+        /*Stream<String> stringStream = stringList.stream().map(name -> name.substring(0,3));
+        stringStream.forEach(System.out::println);
+        stringStream.close();*/
+
+        // filter
+        /*Stream<String> stringStream = stringList.stream().filter(name -> name.startsWith("t"));
+        stringStream.forEach(System.out::println);
+        stringStream.close();*/
+
+        // limit
+        /*Stream<String> stringStream = stringList.stream().limit(2);
+        stringStream.forEach(System.out::println);
+        stringStream.close();*/
+
+        // sort
+        List<String> sortedList = stringList.stream().sorted(Comparator.reverseOrder()).toList();
+        sortedList.forEach(System.out::println);
+
+
     }
 }
