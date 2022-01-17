3c0e649924c14c50f00678883dc380b2dba46dc2
polamreddyvivekreddy <polamreddy.vivek@gmail.com>
2022-01-17 22:49:26 +0530

    reflection demo

--- src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Main.java
@@ -1,9 +1,31 @@
 package io.github.polamreddyvivekreddy.hellojavaworld;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("hello-java-world");
+
+        Class modelObject = Model.class;
+        System.out.println(modelObject.getName());
+
+        // fields
+        System.out.println("-----fields-----");
+        Field[] fields = modelObject.getFields();
+        System.out.println(fields.length);
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println(fields[i]);
+        }
+        // methods
+        System.out.println("----methods----");
+        Method[] methods = modelObject.getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            System.out.println(methods[i]);
+        }
+
+
     }
 }
---
+++ src/io/github/polamreddyvivekreddy/hellojavaworld/Model.java
@@ -0,0 +1,23 @@
+package io.github.polamreddyvivekreddy.hellojavaworld;
+
+public class Model {
+
+    public String modelName;
+    public int modelId;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(int modelId) {
+        this.modelId = modelId;
+    }
+}
